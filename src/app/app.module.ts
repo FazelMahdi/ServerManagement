235ed29ab1b6ade61cc26b80e8cb9bac8ce6564a
Mahdi <mahdifazeli@live.com>
2018-05-26 11:39:47 +0430

    RtlBootstrap Added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { UsersComponent } from './users/users.component';
+import { HomeComponent } from './home/home.component';
+import { UserComponent } from './users/user/user.component';
+import { ServersComponent } from './servers/servers.component';
+import { ServerComponent } from './servers/server/server.component';
+import { EditServerComponent } from './servers/edit-server/edit-server.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UsersComponent,
+    HomeComponent,
+    UserComponent,
+    ServersComponent,
+    ServerComponent,
+    EditServerComponent
   ],
   imports: [
     BrowserModule
